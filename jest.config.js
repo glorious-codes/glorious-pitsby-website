cb1c3bd27adcaf5c55a9c55fe751a70ef8f8c194
Rafael Camargo <hello@rafaelcamargo.com>
2020-01-25 17:55:39 -0300

    chore: create unit tests configuration

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const project = require('./project.json');
+const env = process.NODE_ENV || 'development';
+
+module.exports = {
+  collectCoverageFrom: project.tests.source.files,
+  coverageReporters: ['html'],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  },
+  moduleNameMapper: {
+    '@vue$': 'vue/dist/vue.common.js',
+    '@environment$': `<rootDir>/${project.environments.source.root}/${env}.js`,
+    '@scripts\/(.*)$': `<rootDir>/${project.scripts.source.root}$1`,
+    '@styles\/(.*)$': `<rootDir>/${project.styles.source.root}$1`
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/scripts/base/mocks/global.js'],
+  transform: {
+    '^.+\\.(css|styl)$': '<rootDir>/src/scripts/base/mocks/raw-files.js',
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.html$': 'html-loader-jest'
+  }
+}
