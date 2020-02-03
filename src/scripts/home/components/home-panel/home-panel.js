4dc0d7225763aace7ea1704c51224fecda8a4ea6
Rafael Camargo <hello@rafaelcamargo.com>
2020-02-02 23:59:19 -0300

    feat: apply features list to home panel component

--- src/scripts/home/components/home-panel/home-panel.js
+++ src/scripts/home/components/home-panel/home-panel.js
@@ -1,6 +1,7 @@
 import '@styles/home-panel.styl';
 import pCol from '@scripts/base/components/col/col';
 import pLogo from '@scripts/base/components/logo/logo';
+import pFeaturesList from '@scripts/base/components/features-list/features-list';
 import pRow from '@scripts/base/components/row/row';
 import template from './home-panel.html';
 
@@ -9,6 +10,7 @@ const pHomePanel = {
   components: {
     pCol,
     pLogo,
+    pFeaturesList,
     pRow
   },
   template
