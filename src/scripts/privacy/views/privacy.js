8d5f0a1fa230d84b9ca26dd99c9986d7789969d2
Rafael Camargo <hello@rafaelcamargo.com>
2020-02-15 11:12:47 -0300

    feat: add SEO metatags to privacy view

--- src/scripts/privacy/views/privacy.js
+++ src/scripts/privacy/views/privacy.js
@@ -2,7 +2,19 @@ import pCol from '@scripts/base/components/col/col';
 import pContainer from '@scripts/base/components/container/container';
 import pRow from '@scripts/base/components/row/row';
 import pViewport from '@scripts/base/components/viewport/viewport';
+import seoService from '@scripts/base/services/seo/seo';
 import template from './privacy.html';
+
+const head = seoService.buildHead({
+  title: 'Pitsby: Privacy',
+  description: [
+    'Subscribing on my personal newsletter, you will be notified about',
+    'Pitsby\'s launch as well as updates regarding my personal',
+    'projects or stories. I\'d love to find time to build and write more, but',
+    'don\'t worry, it doesn\'t happen often.'
+  ],
+  keywords: 'pitsby, privacy, subscription'
+});
 
 const privacyView = {
   name: 'p-privacy-view',
