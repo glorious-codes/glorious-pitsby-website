2ffc8886d50d51802cc98d8e5ef59ddedb3d9d74
Rafael Camargo <hello@rafaelcamargo.com>
2020-02-15 11:12:40 -0300

    feat: add SEO metatags to home view

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,16 @@
 import '@styles/_native.styl';
 import Vue from '@vue';
+import VueHead from 'vue-head';
 import VueRouter from 'vue-router';
 import routes from '@scripts/routes';
 import template from '@scripts/app.html';
 import analyticsService from '@scripts/base/services/analytics/analytics';
 import routeService from '@scripts/base/services/route/route';
 
+Vue.use(VueHead, {
+  separator: '',
+  complement: ''
+});
 Vue.use(VueRouter);
 
 const router = new VueRouter({ routes, mode: 'history' });
--- src/scripts/home/views/home.js
+++ src/scripts/home/views/home.js
@@ -4,7 +4,14 @@ import pHomePanel from '@scripts/home/components/home-panel/home-panel';
 import pLogo from '@scripts/base/components/logo/logo';
 import pRow from '@scripts/base/components/row/row';
 import pViewport from '@scripts/base/components/viewport/viewport';
+import seoService from '@scripts/base/services/seo/seo';
 import template from './home.html';
+
+const head = seoService.buildHead({
+  title: 'Pitsby',
+  description: 'The declarative way of documenting components.',
+  keywords: 'pitsby, ui, components, documentation, declarative'
+});
 
 const homeView = {
   name: 'p-home-view',
@@ -16,6 +23,7 @@ const homeView = {
     pRow,
     pViewport
   },
+  head,
   template
 };
 
