134fa0b3b6fd6ad36c58ad54d4117d5b7c1b849d
Rafael Camargo <hello@rafaelcamargo.com>
2020-04-05 19:25:40 -0300

    chore: add topbar to viewport

--- src/scripts/base/components/viewport/viewport.js
+++ src/scripts/base/components/viewport/viewport.js
@@ -1,9 +1,13 @@
 import '@styles/viewport.styl';
+import pTopbar from '@scripts/base/components/topbar/topbar';
 import template from './viewport.html';
 
-const viewport = {
+const pViewport = {
   name: 'p-viewport',
+  components: {
+    pTopbar
+  },
   template
 };
 
-export default viewport;
+export default pViewport;
--- src/scripts/base/components/viewport/viewport.test.js
+++ src/scripts/base/components/viewport/viewport.test.js
@@ -1,4 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
+import pTopbar from '@scripts/base/components/topbar/topbar';
 import viewport from './viewport';
 
 describe('Viewport', () => {
@@ -9,5 +10,10 @@ describe('Viewport', () => {
   it('should have appropriate css class', () => {
     const wrapper = mount();
     expect(wrapper.classes()).toContain('p-viewport');
+  });
+
+  it('should contain topbar', () => {
+    const wrapper = mount();
+    expect(wrapper.findAll(pTopbar).length).toEqual(1);
   });
 });
