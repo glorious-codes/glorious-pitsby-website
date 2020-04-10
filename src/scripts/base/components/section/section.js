cf21e2a265d32a5fd6365e91dffc5f205c8c286a
Rafael Camargo <hello@rafaelcamargo.com>
2020-04-10 17:39:15 -0300

    feat: improve section component
    add theme prop

--- src/scripts/base/components/section/section.js
+++ src/scripts/base/components/section/section.js
@@ -1,7 +1,26 @@
 import '@styles/section.styl';
 import template from './section.html';
 
-export default {
+const pSection = {
   name: 'p-section',
+  props: {
+    theme: {
+      type: String
+    }
+  },
+  computed: {
+    classes(){
+      return {
+        [`p-section-${this.theme}`]: isThemeValid(this.theme)
+      };
+    }
+  },
   template
 };
+
+
+function isThemeValid(theme){
+  return ['secondary'].includes(theme);
+}
+
+export default pSection;
--- src/scripts/base/components/section/section.test.js
+++ src/scripts/base/components/section/section.test.js
@@ -2,12 +2,17 @@ import { shallowMount } from '@vue/test-utils';
 import pSection from './section';
 
 describe('Section', () => {
-  function mount(){
-    return shallowMount(pSection);
+  function mount(propsData = {}){
+    return shallowMount(pSection, { propsData });
   }
 
   it('should have appropriate class', () => {
     const wrapper = mount();
     expect(wrapper.classes()).toContain('p-section');
+  });
+
+  it('should optionally render a secondary section', () => {
+    const wrapper = mount({ theme: 'secondary' });
+    expect(wrapper.classes()).toContain('p-section-secondary');
   });
 });
