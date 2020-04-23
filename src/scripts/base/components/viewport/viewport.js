b70d03497477215bb1d8bb56efc99bc66908cf49
Rafael Camargo <hello@rafaelcamargo.com>
2020-04-22 21:08:45 -0300

    feat: apply footer component to viewport component

--- src/scripts/base/components/viewport/viewport.js
+++ src/scripts/base/components/viewport/viewport.js
@@ -1,11 +1,13 @@
 import '@styles/viewport.styl';
+import pFooter from '@scripts/base/components/footer/footer';
 import pTopbar from '@scripts/base/components/topbar/topbar';
 import template from './viewport.html';
 
 const pViewport = {
   name: 'p-viewport',
   components: {
-    pTopbar
+    pTopbar,
+    pFooter
   },
   template
 };
