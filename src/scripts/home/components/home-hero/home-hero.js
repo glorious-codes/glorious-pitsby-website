0ceaeeb8dcd23a626e9d2b86503b237424af62e3
Rafael Camargo <hello@rafaelcamargo.com>
2020-04-23 21:16:37 -0300

    feat: add github button widget to home hero component

--- src/scripts/home/components/home-hero/home-hero.js
+++ src/scripts/home/components/home-hero/home-hero.js
@@ -1,5 +1,6 @@
 import '@styles/home-hero.styl';
 import pArrowLink from '@scripts/base/components/arrow-link/arrow-link';
+import pGithubButtonWidget from '@scripts/base/components/github-button-widget/github-button-widget';
 import pHero from '@scripts/base/components/hero/hero';
 import pImg from '@scripts/base/components/img/img';
 import template from './home-hero.html';
@@ -8,6 +9,7 @@ const pHomeHero = {
   name: 'p-home-hero',
   components: {
     pArrowLink,
+    pGithubButtonWidget,
     pHero,
     pImg
   },
