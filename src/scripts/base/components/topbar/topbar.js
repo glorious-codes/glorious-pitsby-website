52c361888f968508151aac7ebed3d7b4082e39e0
Rafael Camargo <hello@rafaelcamargo.com>
2020-04-25 18:57:30 -0300

    feat: link topbar logo to home

--- src/scripts/base/components/topbar/topbar.js
+++ src/scripts/base/components/topbar/topbar.js
@@ -1,6 +1,7 @@
 import '@styles/topbar.styl';
 import pCol from '@scripts/base/components/col/col';
 import pContainer from '@scripts/base/components/container/container';
+import pLink from '@scripts/base/components/link/link';
 import pLogo from '@scripts/base/components/logo/logo';
 import pMenu from '@scripts/base/components/menu/menu';
 import pRow from '@scripts/base/components/row/row';
@@ -11,6 +12,7 @@ const pTopbar = {
   components: {
     pCol,
     pContainer,
+    pLink,
     pLogo,
     pMenu,
     pRow
