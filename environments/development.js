e34535c0bab27a4290259c4a8df8dfa0f43a3393
Rafael Camargo <hello@rafaelcamargo.com>
2023-02-16 23:27:37 -0300

    chore: remove unused code
    - remove newsletter form
    - remove newsletter resource
    - remove vervet resource
    - remove base resource
    - remove vervet env vars

--- environments/development.js
+++ environments/development.js
@@ -7,14 +7,5 @@ export default {
       DOMAIN: 'dev.pitsby.com',
       OPTIONS: { trackLocalhost: false }
     }
-  },
-  VERVET: {
-    BASE_URL: 'http://localhost:9001',
-    PROJECTS: {
-      NEWSLETTER: {
-        ID: '569e642a-9f75-41dc-9a96-7ac66bc6eef9',
-        LIST_ID: 'aec1ae3c35'
-      }
-    }
   }
 };
--- environments/production.js
+++ environments/production.js
@@ -6,14 +6,5 @@ export default {
     PLAUSIBLE: {
       DOMAIN: 'pitsby.com'
     }
-  },
-  VERVET: {
-    BASE_URL: 'https://vervetapp.herokuapp.com',
-    PROJECTS: {
-      NEWSLETTER: {
-        ID: 'e02ab6c2-558e-4ab3-bf56-06f290971976',
-        LIST_ID: '1e01434c89'
-      }
-    }
   }
 };
