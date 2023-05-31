d1a52633f331593a22a37b162663b7728798356e
Rafael Camargo <hello@rafaelcamargo.com>
2023-05-31 11:41:06 -0300

    feat: improve adopters list
    improve visual of showcase message

--- src/scripts/base/components/adopters-list/adopters-list.js
+++ src/scripts/base/components/adopters-list/adopters-list.js
@@ -1,5 +1,4 @@
 import '@styles/adopters-list.styl';
-import pAlert from '@scripts/base/components/alert/alert';
 import pLazyLoad from '@scripts/base/components/lazy-load/lazy-load';
 import pLink from '@scripts/base/components/link/link';
 import template from './adopters-list.html';
@@ -7,7 +6,6 @@ import template from './adopters-list.html';
 const pAdoptersList = {
   name: 'p-adopters-list',
   components: {
-    pAlert,
     pLazyLoad,
     pLink
   },
