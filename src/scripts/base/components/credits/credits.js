62840434cbb2fbf20f48fb235990b795dba2aa03
Rafael Camargo <hello@rafaelcamargo.com>
2023-07-27 19:20:19 -0300

    feat: improve footer
    add statement about no-cookies and public analytics

--- src/scripts/base/components/credits/credits.js
+++ src/scripts/base/components/credits/credits.js
@@ -1,7 +1,11 @@
 import '@styles/credits.styl';
+import pLink from '@scripts/base/components/link/link';
 import template from './credits.html';
 
 export default {
   name: 'p-credits',
+  components: {
+    pLink
+  },
   template
 };
